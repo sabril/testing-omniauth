68a7e3d0e55afe2d2a264bd2361d357d31562096
remi <remi@remitaylor.com>
2012-03-04 19:36:46 -0800

    Fresh Rails 3.2.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestingOmniauth::Application.config.secret_token = '55e95c3e5c21775f27ad7a4cc738d11771f27faf3ec7a6a6ee315f5b26f6723a2a7abb16902ee97e6e590956eb20372f5bb8b5dc33386f8a142519e83128e6ff'
